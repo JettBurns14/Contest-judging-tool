0394135467b2c77d026894cf3f3442e8cac99ff7
Jett <jettburns11@icloud.com>
2018-02-28 08:28:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
+
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'html');
 
 app.get('/', function(request, response) {
   response.render('pages/judging');
