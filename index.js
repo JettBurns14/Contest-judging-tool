0a2ce2953a8d803df3586b5fa5179bc03b2b637e
Jett <jettburns11@icloud.com>
2018-03-24 08:34:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,110 +1,267 @@
-var express = require('express');
-var pg = require('pg');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const Request = require('request');
+const Moment = require('moment');
+const CookieParser = require('cookie-parser');
+const pg = require('pg');
 
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+const app = express();
 
 // views is directory for all template files
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+// app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.set('port', (process.env.PORT || 5000));
 
-// Needed?
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'html');
+app.use(CookieParser());
+app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), () => {
     console.log('Node app is running on port', app.get('port'));
 });
 
-// Body parser stuff
 // Ref: https://github.com/expressjs/body-parser
 var jsonParser = bodyParser.json();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-// Post routes
-app.get('/routes/contest-id', (request, response) => {
-    response.send(`var CONTEST_ID = "${process.env.CONTEST_PROGRAM_ID}"`);
-});
+// Specifies where pages are, and what format.
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'html');
+
+// Error handler function.
+function handleError(err, res) {
+    console.error(err);
+    res.status(500).render('pages/error', { error: err });
+};
+
+// Security check.
+function checkAccess(res) {
+    if (res.cookies.hasOwnProperty(process.env.COOKIE_1)) {
+      return true;
+    }
+    return false;
+}
+
+/***  POST ROUTES  ***/
 
 app.post('/routes/judging', urlencodedParser, (request, response) => {
-    if (!request.body) return response.sendStatus(400);
-    console.log('Request received');
-
-    // Try INSERTing the data.
-    var creativity_score = request.body.creativity;
-    var complexity_score = request.body.complexity;
-    var clean_code_score = request.body.clean_code;
-    var originality_score = request.body.originality;
-
-    try {
-        /*pg.connect(process.env.DATABASE_URL, (err, client, done) => {
-            client.query('INSERT INTO ', (err, result) => {
-                done();
-                if (err) {
-                    console.error(err);
-                    response.render('pages/error.html', { error: err });
-                } else {
-                    response.render('pages/dashboard.html', {results: result.rows} );
-                }
-            });
-        });*/
-    } catch(err) {
-        console.error(err);
-        response.render('pages/error.html', { error: err });
+    console.log('/routes/judging was fired.');
+    if (!request.body) {
+        return handleError({ error: 'No request body received.' }, response);
     }
 
+    let entry_id = request.body.entry_id;
+    let user_id = request.cookies[process.env.COOKIE_2];
+    let score_1 = request.body.creativity / 2;
+    let score_2 = request.body.complexity / 2;
+    let score_3 = request.body.quality_code / 2;
+    let score_4 = request.body.interpretation / 2;
 
-    // SQL has callback, just render judging.html with that.
-    setTimeout(() => {
-        response.render('pages/judging');
-    }, 2000);
-});
-
-app.post('/routes/update-entries', (request, response) => {
-    // Update entries table.
-    response.redirect('/judging');
-});
-
-
-// Page routes
-app.get('/', function(request, response) {
-    // Home page for user, maybe info/instructions about tool.
-    response.send('Stuff that goes here is to be determined.');
-});
-
-app.get('/judging', (request, response) => {
-    response.render('pages/judging');
-});
-
-app.get('/admin', (request, response) => {
-    // this pg.connect could be client.connect()
-    // https://node-postgres.com/features/connecting
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        try {
-            client.query('SELECT * FROM test_table', function(err, result) {
+    try {
+        pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+            client.query('SELECT evaluate($1, $2, $3, $4, $5, $6)', [entry_id, user_id, score_1, score_2, score_3, score_4], (err, result) => {
                 done();
                 if (err) {
-                    console.error(err);
-                    response.render('pages/error.html', { error: err });
-                } else {
-                    response.render('pages/admin', { results: result.rows });
+                    return handleError(err, response);
                 }
+                response.redirect('/judging');
             });
-        } catch(err) {
-            console.error(err);
-            response.render('pages/error.html', { error: err });
+        });
+    } catch(err) {
+        handleError(err, response);
+    }
+});
+
+// WIP
+app.post('/routes/update-entries', (request, response) => {
+    console.log('/routes/update-entries was fired.');
+    // https://www.khanacademy.org/api/internal/scratchpads/Scratchpad:5234130249875456/top-forks?sort=2&page=0&limit=1000&format=pretty
+    Request(`https://www.khanacademy.org/api/internal/scratchpads/Scratchpad:${process.env.CONTEST_PROGRAM_ID}/top-forks?sort=2&page=0&limit=1000`, (err, res, body) => {
+        console.log('Request callback called.');
+        if (!JSON.parse(body)) {
+            return handleError({ error: 'No request body received.' }, response);
+        } else
+        if (err) {
+            return handleError(err, response);
+        }
+
+        let data = JSON.parse(body);
+        if (data) {
+            console.log('Data exists.');
+            // When data is received (which is sorted already)...
+            // SELECT all current entries in DB table.
+            // Sort current table entries by created date.
+            // Get most recent entry from table, TOP().
+            try {
+                pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+                    client.query('SELECT MAX(entry_created) FROM entry', (err, res) => {
+                        done();
+                        console.log('Query callback called.');
+                        if (err) {
+                            return handleError(err, response);
+                        }
+                        console.log(`${res.rows[0].max} --- ${data.scratchpads.length} new programs.`);
+                        for (var i = 0; i < data.scratchpads.length; ++i) {
+                            // moment docs: http://momentjs.com/docs/#/query/
+                            if (Moment(res.rows[0].max).isBefore(data.scratchpads[i].created)) {
+                                //console.log(`Program ${data.scratchpads[i].title} was created after ${res.rows[0].max} on ${data.scratchpads[i].created}.`);
+                                console.log(`${data.scratchpads[i].created} - ${data.scratchpads[i].url}`)
+                            }
+                            // console.log(`Program "${data.scratchpads[i].title}" created on ${data.scratchpads[i].created}. ${Moment(res.rows[0].max).isBefore(data.scratchpads[i].created)}`);
+                        }
+                        // Store all API entries created after this returned date.
+                        // Loop through entries, and check if each moment(res.row).isBefore(data[i].created)
+                    });
+                });
+            } catch(err) {
+                handleError(err, response);
+            }
+            // Now compare most recent table entry creation date to all dates in API entries.
+            // If programs have been created since the most recent in table, store them.
+            // Once all have been compared, INSERT stored entries into table.
         }
     });
 });
 
+app.post('/routes/login', urlencodedParser, (request, response) => {
+    console.log('/routes/login was fired.');
+
+    if (!request.cookies.hasOwnProperty(process.env.COOKIE_1)) {
+
+        if (request.body.councilPassword === process.env.COUNCIL_PW) {
+            let userId = request.body.evaluator.split(' - ')[0];
+            let username = request.body.evaluator.split(' - ')[1];
+            let password = request.body.userPassword;
+
+            pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+                client.query('SELECT verify_evaluator($1, $2)', [userId, password], (err, res) => {
+                    done();
+                    if (err) {
+                        return handleError(err, response);
+                    }
+                    if (res.rows[0].verify_evaluator) {
+                        response.cookie(process.env.COOKIE_1, true, { expires: new Date(Date.now() + (1000 * 3600 * 24 * 365))});
+                        response.cookie(process.env.COOKIE_2, userId, { expires: new Date(Date.now() + (1000 * 3600 * 24 * 365))});
+                        response.cookie(process.env.COOKIE_3, username, { expires: new Date(Date.now() + (1000 * 3600 * 24 * 365))});
+
+                        client.query('UPDATE evaluator SET logged_in = true WHERE evaluator_id = $1', [userId], (err, res) => {
+                            if (err) {
+                                return handleError(err, response);
+                            }
+                            response.redirect('/');
+                        });
+                    } else {
+                        response.send('Account password is incorrect.');
+                    }
+                });
+            });
+        } else {
+            response.send('Council password is incorrect.');
+        }
+    } else {
+        response.redirect('/');
+    }
+});
+
+
+/***  PAGE ROUTES  ***/
+app.get('/', function(request, response) {
+    if (!checkAccess(request)) {
+        response.send('Unauthorized');
+    } else {
+        response.render('pages/home');
+    }
+});
+
+app.get('/login', (request, response) => {
+    if (request.cookies.hasOwnProperty(process.env.COOKIE_1)) {
+        return response.redirect('/');
+    }
+    try {
+        pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+            client.query('SELECT evaluator_id, evaluator_name FROM evaluator WHERE logged_in = false ORDER BY evaluator_id', (err, res) => {
+                done();
+                if (err) {
+                    return handleError(err, response);
+                }
+                response.render('pages/login', { evaluators: res.rows });
+            });
+        });
+    } catch(err) {
+        handleError(err, response);
+    }
+});
+
+app.get('/judging', (request, response) => {
+    if (!checkAccess(request)) {
+        return response.send('Unauthorized');
+    }
+    try {
+        let userId = request.cookies[process.env.COOKIE_2];
+        pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+            client.query(`SELECT * FROM get_entry_and_create_placeholder($1)`, [userId], (err, res) => {
+                done();
+                if (err) {
+                    return handleError(err, response);
+                }
+                response.render('pages/judging', { entry: res.rows });
+            });
+        });
+    } catch(err) {
+        handleError(err, response);
+    }
+});
+
+app.get('/admin', (request, response) => {
+    if (!checkAccess(request)) {
+        return response.send('Unauthorized');
+    }
+    try {
+        pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+            var entry1, reviewed1, contests1, entries1;
+
+            client.query('SELECT COUNT(*) FROM entry', (err, res) => {
+                done();
+                if (err) {
+                    return handleError(err, response);
+                }
+                entry1 = res.rows;
+            });
+            client.query('SELECT COUNT(*) FROM evaluation WHERE evaluation_complete = true;', (err, res) => {
+                done();
+                if (err) {
+                    return handleError(err, response);
+                }
+                reviewed1 = res.rows;
+            });
+            client.query('SELECT * FROM contest', (err, res) => {
+                done();
+                if (err) {
+                    return handleError(err, response);
+                }
+                contests1 = res.rows;
+            });
+            client.query('SELECT * FROM entry', (err, res) => {
+                done();
+                if (err) {
+                    return handleError(err, response);
+                }
+                entries1 = res.rows;
+                response.render('pages/admin', { entry1: entry1, reviewed1: reviewed1, contests1: contests1, entries1: entries1 });
+            });
+        });
+    } catch(err) {
+        handleError(err, response);
+    }
+});
+
 app.get('/profile', (request, response) => {
+    if (!checkAccess(request)) {
+        return response.send('Unauthorized');
+    }
     response.render('pages/profile');
 });
 
+// Handler for all errors.
 app.use(function(err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+    handleError(err, res);
 });
