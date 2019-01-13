14a16a84050d23fd1a8e06f9e8841a7ae8111648
Jett Burns <jettburns11@icloud.com>
2019-01-12 20:26:57 -0600

    Update views & CSS, add some images
    Update to login.ejs accommodates OAuth functionality.

--- public/js/login.js
+++ public/js/login.js
@@ -1,36 +1,15 @@
-const submitLogin = e => {
+const submitForm = e => {
     e.preventDefault();
-    var errors = document.getElementsByClassName("error");
-    for (var i = 0; i < errors.length; i++) {
-        errors[i].textContent = "";
-    }
-    var postData = {};
-    for (var i = 0; i < e.target.length - 1; i++) {
-        postData[e.target[i].name] = e.target[i].value;
-    }
-    fetch("/api/login", {
+    fetch("/api/auth/connect", {
         method: "POST",
-        body: JSON.stringify(postData),
         headers: {
             "Content-Type": "application/json"
         }
     }).then(res => {
         return res.json();
     }).then(data => {
-        // Unauthorized login
-        // if (data.code === 1) {
-        //     document.getElementById("evalutor-error").textContent = "Unauthorized";
-        // }
-        // // Council password incorrect
-        // if (data.code === 2) {
-        //     document.getElementById("councilpw-error").textContent = "Incorrect";
-        // }
-        // // Account password incorrect
-        // if (data.code === 3) {
-        //     document.getElementById("password-error").textContent = "Incorrect";
-        // }
-        if (data.code === 4) {
-            window.location.href = window.location.href.slice(0, window.location.href.length - 5);
+        if (data.redirect_url) {
+            window.location.href = data.redirect_url;
         }
-    })
-}
+    });
+};
