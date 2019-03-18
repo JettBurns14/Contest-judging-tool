697ad38104a8194405229a2a5c28928ebc12fbb9
Jett Burns <jettburns11@icloud.com>
2019-03-18 13:47:06 -0500

    Update entries/results views
    - Added JS file for the entries view, no longer setting innerHTML like
    before.
    - Updated entries/results CSS to look like admin dashboard page.
    - Changed a few things in entries/results views HTML.

--- public/js/entries.js
+++ public/js/entries.js
@@ -1,46 +1,53 @@
-var navButtons = document.getElementsByClassName('nav-button');
-navButtons[<%= contest_id %> - 1].classList.add('selected');
+let navButtons = document.getElementsByClassName("nav-button");
+let hasHashInUrl = document.location.href.indexOf("#") != -1;
+let currentContestId = (
+    hasHashInUrl ?
+    document.location.href.split("#")[0].split("/")[4] :
+    document.location.href.split("/")[4]
+);
+navButtons[currentContestId - 1].classList.add("selected");
 
-function deleteEntry(id, contest_id) {
-	var shouldDelete = confirm("Are you sure you want to delete this entry?");
+let deleteEntry = (id, contest_id) => {
+    let shouldDelete = confirm("Are you sure you want to delete this entry?");
 
-	if (shouldDelete) {
-		post('/api/deleteEntry', {
-			'entry_id': id,
-			'contest_id': contest_id
-		});
-	}
+    if (shouldDelete) {
+        post("/api/deleteEntry", {
+            "entry_id": id,
+            "contest_id": contest_id
+        });
+    }
 }
 
-function editEntry(id) {
-	var row = document.getElementById(id);
-	var entryLevelBox = document.getElementById(id + '-level');
-	var currentLevel = entryLevelBox.innerHTML;
-	var actionBox = document.getElementById(id + '-actions');
-
-	entryLevelBox.innerHTML = "<form method='post' action='/api/editEntry'><input type='hidden' name='contest_id' value='<%= contest_id %>'><input type='hidden' name='entry_id' value='" + id + "'><select name='edited_level'><option value='tbd'>TBD</option><option value='Beginner'>Beginner</option><option value='Intermediate'>Intermediate</option><option value='Advanced'>Advanced</option></select><input class='check-icon' type='image' src='../static/images/checkmark.png' border='0' alt='Submit'/></form>";
+let editEntry = (id, elementIndex, contest_id) => {
+    let viewEntryLevel = document.querySelectorAll(".view-entry-level")[elementIndex];
+    let editEntryLevel = document.querySelectorAll(".edit-entry-level")[elementIndex];
+    let editEntryLevelForm = document.querySelectorAll(".edit-entry-level-form")[elementIndex];
+    viewEntryLevel.style.display = "none";
+    editEntryLevel.style.display = "block";
+    editEntryLevelForm[0].value = contest_id;
+    editEntryLevelForm[1].value = id;
 }
 
-function post(path, params, method) {
-	method = method || "post"; // Set method to post by default if not specified.
+let post = (path, params, method) => {
+    method = method || "post"; // Set method to post by default if not specified.
 
-	// The rest of this code assumes you are not using a library.
-	// It can be made less wordy if you use one.
-	var form = document.createElement("form");
-	form.setAttribute("method", method);
-	form.setAttribute("action", path);
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    let form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
 
-	for (var key in params) {
-		if (params.hasOwnProperty(key)) {
-			var hiddenField = document.createElement("input");
-			hiddenField.setAttribute("type", "hidden");
-			hiddenField.setAttribute("name", key);
-			hiddenField.setAttribute("value", params[key]);
+    for (let key in params) {
+        if (params.hasOwnProperty(key)) {
+            let hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
 
-			form.appendChild(hiddenField);
-		}
-	}
+            form.appendChild(hiddenField);
+        }
+    }
 
-	document.body.appendChild(form);
-	form.submit();
+    document.body.appendChild(form);
+    form.submit();
 }
