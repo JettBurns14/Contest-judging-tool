8919d619b6d2c036326565de733e550da97c334e
Evan Lewis <36089251+lewisevan@users.noreply.github.com>
2020-02-10 15:01:37 -0600

    Fixes bug with deleting users
    Deleting whitelisted kaids was not working due to an incorrect API endpoint call.

--- public/js/admin.js
+++ public/js/admin.js
@@ -283,7 +283,7 @@ let whitelistUser = (e) => {
 let deleteWhitelistedUser = (kaid) => {
     let confirm = window.confirm("Are you sure you want to remove this user from the whitelist?");
     if (confirm) {
-        request("delete", "/api/internal/users/whitelist", {
+        request("delete", "/api/internal/whitelist", {
             kaid
         }, (data) => {
             if (!data.error) {
