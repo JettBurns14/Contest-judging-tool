055ef582b057d06f0505686b6056528ac6033a44
lewisevan <36089251+lewisevan@users.noreply.github.com>
2020-03-14 15:22:07 -0700

    Updates admin page routes

--- routes/pages.js
+++ routes/pages.js
@@ -5,7 +5,7 @@ const handlers = require(process.cwd() + "/handlers/pages");
 router.get("/", handlers.home);
 router.get("/login", handlers.login);
 router.get("/judging", handlers.judging);
-router.get("/admin", handlers.admin);
+router.get("/admin/:page", handlers.admin);
 router.get("/results/:contestId", handlers.results);
 router.get("/entries/:contestId", handlers.entries)
 module.exports = router;
