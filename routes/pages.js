3676d1ebcb85e48967d7fb8dd59161e66bf38dfb
lewisevan <36089251+lewisevan@users.noreply.github.com>
2020-03-22 15:59:08 -0700

    Adds local 404 page images

--- routes/pages.js
+++ routes/pages.js
@@ -5,7 +5,11 @@ const handlers = require(process.cwd() + "/handlers/pages");
 router.get("/", handlers.home);
 router.get("/login", handlers.login);
 router.get("/judging", handlers.judging);
-router.get("/admin/:page", handlers.admin);
 router.get("/results/:contestId", handlers.results);
 router.get("/entries/:contestId", handlers.entries)
+router.get("/admin/dashboard", handlers.adminDashboard);
+router.get("/admin/contests", handlers.adminContests);
+router.get("/admin/tasks", handlers.adminTasks);
+router.get("/admin/users", handlers.adminUsers);
+router.get("/admin/judging", handlers.adminJudging);
 module.exports = router;
