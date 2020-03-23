df1ec64a8b3410c9846f5adfc8fa62b728d6021a
lewisevan <36089251+lewisevan@users.noreply.github.com>
2020-03-22 20:51:59 -0500

    Sets default Evaluations page link

--- public/js/adminSidebar.js
+++ public/js/adminSidebar.js
@@ -1,10 +1,12 @@
 let entriesTab = document.getElementById("sidebar-entries");
 let resultsTab = document.getElementById("sidebar-results");
+let evaluationsTab = document.getElementById("sidebar-evaluations");
 
 request("get", "/api/internal/contests/getCurrentContest", null, (data) => {
     if (!data.error) {
         entriesTab.href = "/entries/" + data.currentContest.contest_id;
         resultsTab.href = "/results/" + data.currentContest.contest_id;
+        evaluationsTab.href += ("/" + data.currentContest.contest_id);
     } else {
         alert(data.error.message);
     }
