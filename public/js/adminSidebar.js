111057e4425cc9f545e613508bf7e42828238fdb
lewisevan <36089251+lewisevan@users.noreply.github.com>
2020-03-26 23:20:19 -0500

    Fixes bug with Evaluations tab

--- public/js/adminSidebar.js
+++ public/js/adminSidebar.js
@@ -6,7 +6,21 @@ request("get", "/api/internal/contests/getCurrentContest", null, (data) => {
     if (!data.error) {
         entriesTab.href = "/entries/" + data.currentContest.contest_id;
         resultsTab.href = "/results/" + data.currentContest.contest_id;
-        evaluationsTab.href += ("/" + data.currentContest.contest_id);
+        //evaluationsTab.href += ("/" + data.currentContest.contest_id);
+    } else {
+        alert(data.error.message);
+    }
+});
+
+request("get", "/api/internal/users/id", null, (d) => {
+    if (!d.error) {
+        request("get", "/api/internal/contests/getContestsEvaluatedByUser?userId=" + d.evaluator_id, null, (data) => {
+            if (!data.error) {
+                evaluationsTab.href += ("/" + data.contests[0].contest_id);
+            } else {
+                alert(data.error.message);
+            }
+        });
     } else {
         alert(data.error.message);
     }
